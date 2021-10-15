bf0f23088637777b830c4a6ffc21a974e6b01686
Jarvis <jarvis.tang@foxmail.com>
2021-10-15 14:08:22 +0800

    初始化语言 zh-Hans

--- app/Http/FresnsPanel/FsControllerWeb.php
+++ app/Http/FresnsPanel/FsControllerWeb.php
@@ -55,7 +55,7 @@ class FsControllerWeb extends BaseFrontendController
     // Login Page
     public function index()
     {
-        $lang = request()->input('lang', 'en');
+        $lang = request()->input('lang', 'zh-Hans');
         $data = [
             'lang' => $lang,
             'location' => 'action',
@@ -69,7 +69,7 @@ class FsControllerWeb extends BaseFrontendController
     // After Login Status Expires
     public function loginIndex()
     {
-        $lang = request()->input('lang', 'en');
+        $lang = request()->input('lang', 'zh-Hans');
         $data = [
             'lang' => $lang,
         ];
@@ -107,9 +107,9 @@ class FsControllerWeb extends BaseFrontendController
 
         Auth::login($user);
 
-        $lang = $request->input('lang', 'en');
+        $lang = $request->input('lang', 'zh-Hans');
         if (empty($lang)) {
-            $lang = 'en';
+            $lang = 'zh-Hans';
         }
         Cache::forever('lang_tag_'.$user['id'], $lang);
 
@@ -195,7 +195,7 @@ class FsControllerWeb extends BaseFrontendController
     // Setting Language
     public function setLanguage(Request $request)
     {
-        $lang = $request->input('lang', 'en');
+        $lang = $request->input('lang', 'zh-Hans');
         $userId = Auth::id();
 
         Cache::forever('lang_tag_'.$userId, $lang);
