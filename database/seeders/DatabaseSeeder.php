6abc4224e228ff1fc8466dd619812abef9fa73a2
Jarvis <jarvis.tang@foxmail.com>
2021-11-05 15:10:40 +0800

    Initial seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        $this->call(CodeMessagesTableSeeder::class);
+        $this->call(ConfigsTableSeeder::class);
+        $this->call(DomainsTableSeeder::class);
+        $this->call(EmojisTableSeeder::class);
+        $this->call(LanguagesTableSeeder::class);
+        $this->call(MemberRolesTableSeeder::class);
+        $this->call(PluginUsagesTableSeeder::class);
+    }
+}
