3ecc201d9a59ad0e67b2cfac249cc7c45e8a756d
hyperbolaa <yuchong321@126.com>
2021-11-08 21:42:55 +0800

    chore: upgrade

---
+++ database/seeders/UpgradeSeeder.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: yuchong
+ * Date: 2021/11/8
+ */
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UpgradeSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //$this->call(CodeMessagesTableSeeder::class);
+    }
+}
