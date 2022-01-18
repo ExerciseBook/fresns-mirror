3d89bd3be5694de11c6e5f28c7f7342542d791cd
liyu <liyu001989@gmail.com>
2022-01-18 16:59:14 +0800

    feat(panel):管理扩展

--- app/Fresns/Panel/Http/Controllers/ExpandManageController.php
+++ app/Fresns/Panel/Http/Controllers/ExpandManageController.php
@@ -3,21 +3,118 @@
 namespace App\Fresns\Panel\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Config;
+use App\Models\Plugin;
+use App\Models\Language;
+use App\Models\PluginUsage;
+use App\Models\MemberRole;
 
 class ExpandManageController extends Controller
 {
     public function index()
     {
-        return view('panel::expand.manage');
+        $plugins = Plugin::all();
+
+        $plugins = $plugins->filter(function ($plugin) {
+            return in_array('pay', $plugin->scene);
+        });
+
+        $pluginUsages = PluginUsage::where('type', 5)
+            ->with('plugin', 'names')
+            ->paginate();
+
+        $memberRoles = MemberRole::all();
+
+
+        return view('panel::expand.manage', compact('pluginUsages', 'plugins', 'memberRoles'));
     }
 
     public function store(Request $request)
     {
+        $pluginUsage = new PluginUsage;
+        $pluginUsage->type = 5;
+        $pluginUsage->name = $request->names[$this->defaultLanguage] ?? (current(array_filter($request->names)) ?: '');
+        $pluginUsage->plugin_unikey = $request->plugin_unikey;
+        $pluginUsage->parameter = $request->parameter;
+        $pluginUsage->is_enable = $request->is_enable;
+        $pluginUsage->rank_num = $request->rank_num;
+        $pluginUsage->is_group_admin = $request->is_group_admin;
+        $pluginUsage->member_roles = $request->member_roles?implode(',', $request->member_roles):$pluginUsage->member_roles;
+        $pluginUsage->scene = $request->scene?implode(',', $request->scene):'';
+        $pluginUsage->save();
+
+        if ($request->update_name) {
+            foreach ($request->names as $langTag => $content) {
+                $language = Language::tableName('plugin_usages')
+                    ->where('table_id', $pluginUsage->id)
+                    ->where('lang_tag', $langTag)
+                    ->first();
+
+                if (!$language) {
+                    // create but no content
+                    if (!$content) {
+                        continue;
+                    }
+                    $language = new Language();
+                    $language->fill([
+                        'table_name' => 'plugin_usages',
+                        'table_field' => 'name',
+                        'table_id' => $pluginUsage->id,
+                        'lang_tag' => $langTag,
+                    ]);
+                }
+
+                $language->lang_content = $content;
+                $language->save();
+            }
+        }
+
+
         return $this->createSuccess();
     }
 
-    public function update()
+    public function update($id, Request $request)
     {
+        $pluginUsage = PluginUsage::find($id);
+        if (!$pluginUsage) {
+            return $this->updateSuccess();
+        }
+        $pluginUsage->name = $request->names[$this->defaultLanguage] ?? (current(array_filter($request->names)) ?: '');
+        $pluginUsage->plugin_unikey = $request->plugin_unikey;
+        $pluginUsage->parameter = $request->parameter;
+        $pluginUsage->is_enable = $request->is_enable;
+        $pluginUsage->rank_num = $request->rank_num;
+        $pluginUsage->is_group_admin = $request->is_group_admin;
+        $pluginUsage->member_roles = $request->member_roles?implode(',', $request->member_roles):$pluginUsage->member_roles;
+        $pluginUsage->scene = $request->scene?implode(',', $request->scene):'';
+        $pluginUsage->save();
+
+        if ($request->update_name) {
+            foreach ($request->names as $langTag => $content) {
+                $language = Language::tableName('plugin_usages')
+                    ->where('table_id', $pluginUsage->id)
+                    ->where('lang_tag', $langTag)
+                    ->first();
+
+                if (!$language) {
+                    // create but no content
+                    if (!$content) {
+                        continue;
+                    }
+                    $language = new Language();
+                    $language->fill([
+                        'table_name' => 'plugin_usages',
+                        'table_field' => 'name',
+                        'table_id' => $pluginUsage->id,
+                        'lang_tag' => $langTag,
+                    ]);
+                }
+
+                $language->lang_content = $content;
+                $language->save();
+            }
+        }
+
         return $this->updateSuccess();
     }
 
--- app/Fresns/Panel/Resources/views/expand/manage.blade.php
+++ app/Fresns/Panel/Resources/views/expand/manage.blade.php
@@ -12,7 +12,10 @@
 	</div>
 	<div class="col-lg-3">
 		<div class="input-group mt-2 mb-4 justify-content-lg-end">
-			<button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#createModal"><i class="bi bi-plus-circle-dotted"></i> 新增扩展</button>
+			<button class="btn btn-primary" type="button"
+			data-bs-toggle="modal"
+			data-action="{{ route('panel.expandManage.store') }}"
+			data-bs-target="#createModal"><i class="bi bi-plus-circle-dotted"></i> 新增扩展</button>
 			<a class="btn btn-outline-secondary" href="#" role="button">帮助说明</a>
 		</div>
 	</div>
@@ -34,64 +37,75 @@
 			</tr>
 		</thead>
 		<tbody>
+			@foreach($pluginUsages as $item)
 			<tr>
-				<td><input type="number" class="form-control input-number" value="1"></td>
-				<td>投诉插件</td>
-				<td><img src="../assets/images/temp/placeholder_icon.png" width="24" height="24"> 投诉</td>
-				<td><span class="badge bg-light text-dark">帖子</span> <span class="badge bg-light text-dark">评论</span> <span class="badge bg-light text-dark">用户</span></td>
-				<td></td>
-				<td><i class="bi bi-check-lg text-success"></i></td>
-				<td></td>
-				<td><i class="bi bi-check-lg text-success"></i></td>
+				<td><input type="number" class="form-control input-number" value="{{ $item['rank_num']}}"></td>
+				<td>{{ optional($item->plugin)->name }}</td>
 				<td>
-					<button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#createModal">修改</button>
+					@if($item->icon_file_url)
+					<img src="{{ $item->icon_file_url }}" width="24" height="24">
+					@endif
+					{{ $item['name'] }}
+				</td>
+				<td>
+					@if(in_array(1,explode(',',$item->scene)))
+						<span class="badge bg-light text-dark">帖子</span>
+					@endif
+					@if(in_array(2,explode(',',$item->scene)))
+						<span class="badge bg-light text-dark">评论</span>
+					@endif
+					@if(in_array(3,explode(',',$item->scene)))
+						<span class="badge bg-light text-dark">用户</span>
+					@endif
+				</td>
+				<td>
+					@foreach($memberRoles as $role)
+						@if(in_array($role->id,explode(',',$item->member_roles)))
+					 		<span class="badge bg-light text-dark">{{$role->name}}</span>
+						@endif
+					@endforeach
+				</td>
+				<td>
+					@if($item['is_group_admin'])
+					  <i class="bi bi-check-lg text-success"></i>
+					@else
+					  <i class="bi bi-dash-lg text-secondary"></i>
+					@endif
+				</td>
+				<td>{{ $item->parameter }}</td>
+				<td>
+					@if($item['is_enable'])
+					  <i class="bi bi-check-lg text-success"></i>
+					@else
+					  <i class="bi bi-dash-lg text-secondary"></i>
+					@endif
+				</td>
+				<td>
+					<button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal"
+					data-names="{{ $item->names->toJson() }}"
+					data-params="{{ json_encode($item->attributesToArray()) }}"
+					data-action="{{ route('panel.expandManage.update', $item->id) }}"
+				 	data-bs-target="#createModal">修改</button>
 					<button type="button" class="btn btn-link link-danger ms-1 fresns-link fs-7">删除</button>
 				</td>
 			</tr>
-			<tr>
-				<td><input type="number" class="form-control input-number" value="2"></td>
-				<td>表单插件</td>
-				<td><img src="../assets/images/temp/placeholder_icon.png" width="24" height="24"> 有奖投稿</td>
-				<td><span class="badge bg-light text-dark">帖子</span></td>
-				<td><span class="badge bg-light text-dark">管理员</span></td>
-				<td><i class="bi bi-check-lg text-success"></i></td>
-				<td>contribute</td>
-				<td><i class="bi bi-dash-lg text-secondary"></i></td>
-				<td>
-					<button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#createModal">修改</button>
-					<button type="button" class="btn btn-link link-danger ms-1 fresns-link fs-7">删除</button>
-				</td>
-			</tr>
-			<tr>
-				<td><input type="number" class="form-control input-number" value="3"></td>
-				<td>表单插件</td>
-				<td><img src="../assets/images/temp/placeholder_icon.png" width="24" height="24"> 建议反馈</td>
-				<td><span class="badge bg-light text-dark">帖子</span> <span class="badge bg-light text-dark">评论</span> <span class="badge bg-light text-dark">用户</span></td>
-				<td></td>
-				<td><i class="bi bi-dash-lg text-secondary"></i></td>
-				<td>feedback</td>
-				<td><i class="bi bi-dash-lg text-secondary"></i></td>
-				<td>
-					<button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#createModal">修改</button>
-					<button type="button" class="btn btn-link link-danger ms-1 fresns-link fs-7">删除</button>
-				</td>
-			</tr>
+			@endforeach
 		</tbody>
 	</table>
 </div>
 <!--操作列表 结束-->
 <nav aria-label="Page navigation example">
 	<ul class="pagination">
-		<li class="page-item disabled"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
-		<li class="page-item active"><a class="page-link" href="#">1</a></li>
-		<li class="page-item"><a class="page-link" href="#">2</a></li>
-		<li class="page-item"><a class="page-link" href="#">3</a></li>
-		<li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
+			{!! $pluginUsages->render() !!}
 	</ul>
 </nav>
 
 <!-- Create Modal -->
-<div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModal" aria-hidden="true">
+<form action="" method="post">
+  @csrf
+  @method('post')
+<input type="hidden" name="update_name" value="0">
+<div class="modal fade  name-lang-parent expend-manage-modal" id="createModal" tabindex="-1" aria-labelledby="createModal" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
@@ -99,172 +113,146 @@
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
 			<div class="modal-body">
-				<form>
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">显示顺序</label>
-						<div class="col-sm-9">
-							<input type="number" class="form-control input-number">
+				<div class="mb-3 row">
+					<label class="col-sm-3 col-form-label">显示顺序</label>
+					<div class="col-sm-9">
+						<input type="number" class="form-control input-number" name="rank_num" required>
+					</div>
+				</div>
+				<div class="mb-3 row">
+					<label class="col-sm-3 col-form-label">关联插件</label>
+					<div class="col-sm-9">
+						<select class="form-select"  name="plugin_unikey" required>
+							<option selected disabled>请选择插件</option>
+							@foreach($plugins as $plugin)
+		                      <option value="{{ $plugin->unikey }}">{{ $plugin->name }}</option>
+		                    @endforeach
+						</select>
+					</div>
+				</div>
+				<div class="mb-3 row">
+					<label class="col-sm-3 col-form-label">显示图标</label>
+					<div class="col-sm-9">
+						<input type="file" class="form-control" id="inputGroupFile01">
+					</div>
+				</div>
+				<div class="mb-3 row">
+					<label class="col-sm-3 col-form-label">显示名称</label>
+					<div class="col-sm-9">
+						<button type="button" class="btn btn-outline-secondary btn-modal w-100 text-start"
+						data-parent="#createModal"
+						data-bs-toggle="modal"
+						data-bs-target="#langModal">显示名称</button>
+					</div>
+				</div>
+				<div class="mb-3 row">
+					<label class="col-sm-3 col-form-label">使用场景</label>
+					<div class="col-sm-9 pt-2">
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="1" name="scene[]">
+							<label class="form-check-label" for="inlineCheckbox1">帖子</label>
+						</div>
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="2" name="scene[]">
+							<label class="form-check-label" for="inlineCheckbox2">评论</label>
+						</div>
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="3" name="scene[]">
+							<label class="form-check-label" for="inlineCheckbox3">用户</label>
 						</div>
 					</div>
+				</div>
+				<div class="mb-3 row">
+					<label class="col-sm-3 col-form-label">使用范围</label>
+					<div class="col-sm-9 pt-2">
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="radio" name="is_group_admin" id="usage_role" value="1" data-bs-toggle="collapse" data-bs-target="#usage_setting:not(.show)" aria-expanded="false" aria-controls="usage_setting" checked>
+							<label class="form-check-label" for="usage_role">全局</label>
+						</div>
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="radio" name="is_group_admin" id="usage_group" value="0" data-bs-toggle="collapse" data-bs-target="#usage_setting.show" aria-expanded="false" aria-controls="usage_setting">
+							<label class="form-check-label" for="usage_group">小组管理员专用</label>
+						</div>
+					</div>
+				</div>
+				<div class="collapse show" id="usage_setting">
 					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">关联插件</label>
+						<label class="col-sm-3 col-form-label">角色使用权</label>
 						<div class="col-sm-9">
-							<select class="form-select">
-								<option selected disabled>请选择插件</option>
-								<option value="1">One</option>
-								<option value="2">Two</option>
-								<option value="3">Three</option>
+							<select class="form-select" multiple name="member_roles[]" id='member_roles'>
+								<option selected>全部</option>
+								@foreach($memberRoles as $role)
+			                      <option value="{{ $role->id }}">{{ $role->name }}</option>
+			                    @endforeach
 							</select>
 						</div>
 					</div>
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">显示图标</label>
-						<div class="col-sm-9">
-							<input type="file" class="form-control" id="inputGroupFile01">
+				</div>
+				<div class="mb-3 row">
+					<label class="col-sm-3 col-form-label">自定义参数</label>
+					<div class="col-sm-9">
+						<input type="text" class="form-control"  name="parameter">
+					</div>
+				</div>
+				<div class="mb-3 row">
+					<label class="col-sm-3 col-form-label">启用状态</label>
+					<div class="col-sm-9 pt-2">
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="radio" name="is_enable" id="status_true" value="1" checked>
+							<label class="form-check-label" for="status_true">启用</label>
+						</div>
+						<div class="form-check form-check-inline">
+							<input class="form-check-input" type="radio" name="is_enable" id="status_false" value="0">
+							<label class="form-check-label" for="status_false">不启用</label>
 						</div>
 					</div>
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">显示名称</label>
-						<div class="col-sm-9">
-							<button type="button" class="btn btn-outline-secondary btn-modal w-100 text-start" data-bs-toggle="modal" data-bs-target="#langModal">显示名称</button>
-						</div>
-					</div>
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">使用场景</label>
-						<div class="col-sm-9 pt-2">
-							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-								<label class="form-check-label" for="inlineCheckbox1">帖子</label>
-							</div>
-							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-								<label class="form-check-label" for="inlineCheckbox2">评论</label>
-							</div>
-							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option2">
-								<label class="form-check-label" for="inlineCheckbox3">用户</label>
-							</div>
-						</div>
-					</div>
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">使用范围</label>
-						<div class="col-sm-9 pt-2">
-							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="usages" id="usage_role" value="true" data-bs-toggle="collapse" data-bs-target="#usage_setting:not(.show)" aria-expanded="false" aria-controls="usage_setting" checked>
-								<label class="form-check-label" for="usage_role">全局</label>
-							</div>
-							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="usages" id="usage_group" value="false" data-bs-toggle="collapse" data-bs-target="#usage_setting.show" aria-expanded="false" aria-controls="usage_setting">
-								<label class="form-check-label" for="usage_group">小组管理员专用</label>
-							</div>
-						</div>
-					</div>
-					<div class="collapse show" id="usage_setting">
-						<div class="mb-3 row">
-							<label class="col-sm-3 col-form-label">角色使用权</label>
-							<div class="col-sm-9">
-								<select class="form-select" multiple>
-									<option selected>全部</option>
-									<option value="1">管理员</option>
-									<option value="2">版主</option>
-									<option value="3">普通会员</option>
-									<option value="4">中级会员</option>
-									<option value="5">高级会员</option>
-								</select>
-							</div>
-						</div>
-					</div>
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">自定义参数</label>
-						<div class="col-sm-9">
-							<input type="text" class="form-control">
-						</div>
-					</div>
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label">启用状态</label>
-						<div class="col-sm-9 pt-2">
-							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="status" id="status_true" value="true" checked>
-								<label class="form-check-label" for="status_true">启用</label>
-							</div>
-							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="status" id="status_false" value="false">
-								<label class="form-check-label" for="status_false">不启用</label>
-							</div>
-						</div>
-					</div>
-					<div class="mb-3 row">
-						<label class="col-sm-3 col-form-label"></label>
-						<div class="col-sm-9"><button type="submit" class="btn btn-primary">提交</button></div>
-					</div>
-				</form>
+				</div>
+				<div class="mb-3 row">
+					<label class="col-sm-3 col-form-label"></label>
+					<div class="col-sm-9"><button type="submit" class="btn btn-primary">提交</button></div>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
 
 <!-- Language Modal -->
-<div class="modal fade" id="langModal" tabindex="-1" aria-labelledby="langModal" aria-hidden="true">
-	<div class="modal-dialog modal-lg">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title">多语言设置</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-			</div>
-			<div class="modal-body">
-				<form>
-					<div class="table-responsive">
-						<table class="table table-hover align-middle text-nowrap">
-							<thead>
-								<tr class="table-info">
-									<th scope="col" class="w-25">语言标签</th>
-									<th scope="col" class="w-25">语言名称</th>
-									<th scope="col" class="w-50">内容</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>zh-Hans-CN</td>
-									<td>简体中文(内地)</td>
-									<td><input type="text" class="form-control" value="显示名称"></td>
-								</tr>
-								<tr>
-									<td>zh-Hans-SG</td>
-									<td>简体中文(新加坡)</td>
-									<td><input type="text" class="form-control" value="显示名称"></td>
-								</tr>
-								<tr>
-									<td>zh-Hans-HK</td>
-									<td>繁體中文(香港)</td>
-									<td><input type="text" class="form-control" value="顯示名稱"></td>
-								</tr>
-								<tr>
-									<td>en-US <i class="bi bi-info-circle text-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="默认语言"></i></td>
-									<td>English(United States)</td>
-									<td><input type="text" class="form-control" value="display name"></td>
-								</tr>
-								<tr>
-									<td>ja</td>
-									<td>日本語</td>
-									<td><input type="text" class="form-control" value="表示名"></td>
-								</tr>
-								<tr>
-									<td>ko-KR</td>
-									<td>한국어(대한민국)</td>
-									<td><input type="text" class="form-control" value="이름 표시하기"></td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-					<!--保存按钮-->
-					<div class="text-center">
-						<button type="button" class="btn btn-success" data-bs-dismiss="modal" aria-label="Close">确认</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+<div class="modal fade name-lang-modal" id="langModal" tabindex="-1" aria-labelledby="langModal" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">多语言设置</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="table-responsive">
+          <table class="table table-hover align-middle text-nowrap">
+            <thead>
+              <tr class="table-info">
+                <th scope="col" class="w-25">语言标签</th>
+                <th scope="col" class="w-25">语言名称</th>
+                <th scope="col" class="w-50">内容</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($optionalLanguages as $lang)
+                <tr>
+                  <td>{{ $lang['langTag'] }}</td>
+                  <td>{{$lang['langName']}} @if($lang['areaCode'])({{ optional($areaCodes->where('code', $lang['areaCode'])->first())['localName']}}) @endif</td>
+                  <td><input type="text" name="names[{{ $lang['langTag'] }}]" class="form-control" value="{{ $langParams['site_name'][$lang['langTag']] ?? '' }}"></td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+        <!--保存按钮-->
+        <div class="text-center">
+          <button type="button" class="btn btn-success" data-bs-dismiss="modal" aria-label="Close">确认</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
+</form>
 
 @endsection
