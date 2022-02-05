faaebc858b45a453eb8696fcf1c1ac173e43524b
liyu <liyu001989@gmail.com>
2022-02-05 20:15:58 +0800

    fix(panel): 站点设置bug

--- app/Fresns/Panel/Resources/views/system/site.blade.php
+++ app/Fresns/Panel/Resources/views/system/site.blade.php
@@ -277,7 +277,9 @@
                       @endif
                     </td>
                     <td>{{$lang['langName']}} @if($lang['areaCode'])({{ optional($areaCodes->where('code', $lang['areaCode'])->first())['localName']}}) @endif</td>
-                    <td><input type="text" name="languages[{{ $lang['langTag'] }}]" class="form-control" value="{{ $langParams['site_desc'][$lang['langTag']] ?? '' }}"></td>
+                    <td>
+                      <textarea name="languages[{{ $lang['langTag'] }}]" class="form-control" rows="3">{{ $langParams['site_desc'][$lang['langTag']] ?? '' }}</textarea>
+                    </td>
                   </tr>
                   @endforeach
                 </tbody>
