8956da03f05cba450cf75a96b98eb6134f63ad16
mouyong <my24251325@gmail.com>
2022-05-23 21:21:55 +0800

    feat: 完成内容替换

--- app/Utilities/ContentUtility.php
+++ app/Utilities/ContentUtility.php
@@ -11,6 +11,7 @@ namespace App\Helpers;
 use App\Models\DomainLink;
 use App\Models\Mention;
 use App\Models\User;
+use Illuminate\Support\Collection;
 
 class ContentUtility
 {
@@ -119,14 +120,17 @@ class ContentUtility
 
         $config = ConfigHelper::fresnsConfigByItemKeys(['site_domain', 'hashtag_show']);
 
+        $replaceList = [];
         $linkList = [];
         foreach ($hashtagList as $hashTag) {
             if ($config['hashtag_show'] == 1) {
                 // 格式 <a href="https://abc.com/hashtag/PHP%E8%AF%AD%E8%A8%80" class="fresns_hashtag" target="_blank">#PHP语言</a>
                 $topic = "#{$hashTag}";
+                $replaceList[] = "$topic ";
             } else {
                 // 格式 <a href="https://abc.com/hashtag/PHP%E8%AF%AD%E8%A8%80" class="fresns_hashtag" target="_blank">#PHP语言#</a>
                 $topic = "#{$hashTag}#";
+                $replaceList[] = "$topic";
             }
 
             $link = sprintf(
@@ -139,7 +143,7 @@ class ContentUtility
             $linkList[] = $link;
         }
 
-        return str_replace($hashtagList, $linkList, $content);
+        return str_replace($replaceList, $linkList, $content);
     }
 
     // Replace url
@@ -149,6 +153,7 @@ class ContentUtility
 
         $urlDataList = DomainLink::whereIn('link_url', $urlList)->get();
 
+        $replaceList = [];
         $linkList = [];
         foreach ($urlList as $url) {
             if ($urlData = $urlDataList->where('', $url)->first()) {
@@ -159,10 +164,11 @@ class ContentUtility
                 $name = $url;
             }
 
+            $replaceList[] = "{$url} ";
             $linkList[] = sprintf('<a href="%s" class="fresns_link" target="_blank">%s</a>', $url, $name);
         }
 
-        return str_replace($urlList, $linkList, $content);
+        return str_replace($replaceList, $linkList, $content);
     }
 
     // Replace mention
@@ -170,41 +176,85 @@ class ContentUtility
     {
         $config = ConfigHelper::fresnsConfigByItemKeys(['site_domain', 'user_identifier']);
         $userList = ContentUtility::extractMention($content);
-        $userDataList = User::where('username', $userList)->get();
+
+        $userDataList = User::whereIn('username', $userList)->get();
+        /** @var Collection $mentionUserId */
         $mentionUserId = Mention::where('linked_type', $linkedType)->where('linked_id', $linkedId)->get();
 
+        // xxx: 不能直接比对，需要通过集合查数据方式处理
+        // 1. 获取所有用户名
+        // 2. 更具用户昵称查找到当前用户
+        // 3. 根据查找到的用户 id 去帖子被 at 列表差是否有记录。无记录说明用户改过昵称，查到的昵称不是之前被 at 时的昵称
         // userDataList['userId'] == $mentionUserId['mention_user_id']
 
-        if ($config['user_identifier'] == 'uid') {
-            // 格式 <a href="https://abc.com/u/{uid}" class="fresns_user" target="_blank">@昵称</a>
-        } else {
-            // 格式 <a href="https://abc.com/u/{username}" class="fresns_user" target="_blank">@昵称</a>
+        $linkList = [];
+        $replaceList = [];
+        foreach ($userList as $username) {
+            $replaceList[] = "@{$username} ";
+
+            // 用户已改昵称，userid 对不上之前被 at 的用户
+            $user = $userDataList->where('username')->first();
+            $mentionUser = $mentionUserId->where('mention_user_id', $user?->id)->first();
+
+            if (is_null($mentionUser)) {
+                // todo: 用户改名后的提示页面链接
+                $linkList[] = sprintf('<a href="%s/u/404" class="fresns_user" target="_blank">@%s</a>', $config['site_domain'], $username);
+                continue;
+            }
+
+            if ($config['user_identifier'] == 'uid') {
+                // 格式 <a href="https://abc.com/u/{uid}" class="fresns_user" target="_blank">@昵称</a>
+                $name = $user->uid;
+            } else {
+                // 格式 <a href="https://abc.com/u/{username}" class="fresns_user" target="_blank">@昵称</a>
+                $name = $user->username;
+            }
+
+            $linkList[] = sprintf('<a href="%s/u/%s" class="fresns_user" target="_blank">@%s</a>', $config['site_domain'], $name, $username);
         }
 
-        return '';
+        return str_replace($replaceList, $linkList, $content);
     }
 
     // Replace sticker
     public static function replaceSticker(string $content): string
     {
         $stickerList = ContentUtility::extractMention($content);
-        $stickerDataList = Mention::where('code', $stickerList)->get();
+        // xxx: code 字段不存在？
+        // @see: https://fresns.cn/database/interactives/mentions.html
+        $stickerDataList = Mention::whereIn('code', $stickerList)->get();
 
-        $stickerUrl = FileHelper::fresnsFileImageUrlByColumn($sticker->image_file_id, $sticker->image_file_url);
+        $replaceList = [];
+        $linkList = [];
+        foreach ($stickerList as $sticker) {
+            $replaceList[] = "[$sticker]";
 
-        // 格式 <img src="$stickerUrl" class="fresns_sticker" alt="$sticker->code">
+            $currentSticker = $stickerDataList->where('code', $sticker)->first();
+            if (is_null($currentSticker)) {
+                $linkList[] = "[$sticker]";
+            } else {
+                $stickerUrl = FileHelper::fresnsFileImageUrlByColumn($sticker->image_file_id, $sticker->image_file_url);
 
-        return '';
+                // 格式 <img src="$stickerUrl" class="fresns_sticker" alt="$sticker->code">
+                $linkList[] = sprintf('<img src="%s" class="fresns_sticker" alt="%s" />', $stickerUrl, $currentSticker->code);
+            }
+        }
+
+        return str_replace($replaceList, $linkList, $content);
     }
 
     // Content
-    public static function contentHandle(string $content, ?int $linkedType = null, ?int $linkedId = null): string
+    public static function contentHandle(string $content, int $linkedType = null, int $linkedId = null): string
     {
         // Replace hashtag
         // Replace url
         // Replace mention
         // Replace sticker
+        $content = static::replaceHashtag($content);
+        $content = static::replaceUrl($content);
+        $content = static::replaceMention($content, $linkedType, $linkedId);
+        $content = static::replaceSticker($content);
 
-        return '';
+        return $content;
     }
 }
