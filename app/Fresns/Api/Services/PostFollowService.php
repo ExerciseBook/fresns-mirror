7fecedc1a31bb9e51fda428e9a6a27a839aef332
Jarvis <jarvis.okay@gmail.com>
2022-06-06 15:37:18 +0800

    feat(api): post follow service

--- app/Fresns/Api/Services/PostFollowService.php
+++ app/Fresns/Api/Services/PostFollowService.php
@@ -9,200 +9,212 @@
 namespace App\Fresns\Api\Services;
 
 use App\Models\Post;
-use App\Models\User;
+use App\Models\UserBlock;
 use App\Models\UserFollow;
-use App\Exceptions\ApiException;
-use App\Models\HashtagLinked;
-use App\Fresns\Api\Services\PostService;
-use App\Models\Hashtag;
-use App\Utilities\AppUtility;
 use App\Utilities\PermissionUtility;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class PostFollowService
 {
-    /**
-     * @var User $user
-     */
-    protected $user;
-
-    protected $userId;
-
-    protected $postService;
-
-    protected $dtoRequest;
-
-    public function __construct($user, $dtoRequest = null)
+    // get post list by follow all
+    public function getPostListByFollowAll(int $authUserId, ?string $contentType = null, ?string $dateLimit = null)
     {
-        $this->user = $user;
+        $followUserIds = UserFollow::type(UserFollow::TYPE_USER)->where('user_id', $authUserId)->pluck('follow_id')->toArray();
+        $allUserIds = Arr::prepend($followUserIds, $authUserId);
+        $followGroupIds = UserFollow::type(UserFollow::TYPE_GROUP)->where('user_id', $authUserId)->pluck('follow_id')->toArray();
+        $followHashtagIds = UserFollow::type(UserFollow::TYPE_HASHTAG)->where('user_id', $authUserId)->pluck('follow_id')->toArray();
 
-        $this->userId = $user?->id;
-        $this->postService = new PostService();
-        $this->dtoRequest = $dtoRequest;
-    }
+        $blockUserIds = UserBlock::type(UserBlock::TYPE_USER)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+        $blockGroupIds = UserBlock::type(UserBlock::TYPE_GROUP)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+        $blockHashtagIds = UserBlock::type(UserBlock::TYPE_HASHTAG)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+        $blockPostIds = UserBlock::type(UserBlock::TYPE_POST)->where('user_id', $authUserId)->pluck('block_id')->toArray();
 
-    public function handle()
-    {
-        $userConfig = PermissionUtility::getUserExpireInfo($this->userId);
-        if (! $userConfig['userStatus'] && $userConfig['expireAfter'] == 1) {
-            throw new ApiException(35302);
-        }
+        $filterGroupIds = PermissionUtility::getGroupPostFilterIds($authUserId);
+        $filterGroupIdsArr = Arr::prepend($blockGroupIds, $filterGroupIds);
 
-        // getAllFollow、getUserFollow、getGroupFollow、getHashtagFollow
-        $method = sprintf("get%sFollow", Str::studly($this->dtoRequest->type));
-
-        return $this->$method();
-    }
-
-    public function getPostList($posts, ?string $followType = null, ?callable $callable = null, ?Collection $hashtagLinkeds = null)
-    {
-        $hashtags = null;
-        if ($hashtagLinkeds?->isNotEmpty()) {
-            $hashtagIds = $hashtagLinkeds->pluck('hashtag_id')->toArray();
-
-            $hashtags = Hashtag::whereIn('id', $hashtagIds)->isEnable()->get();
-        }
-
-        
-        $postList = [];
-        foreach ($posts as $post) {
-            $postHashtags = null;
-
-            if ($hashtags) {
-                $postHashtagIds = $hashtagLinkeds->where('linked_id', $post->id)->pluck('hashtag_id')->toArray();
-                $postHashtags = $hashtags->whereIn('id', $postHashtagIds);
-            }
-            
-            $postItem = $this->postService->postDetail($post, 'list', $this->dtoRequest->mapId, $this->dtoRequest->mapLng, $this->dtoRequest->mapLat, $postHashtags);
-            $postItem['followType'] = $followType;
-
-            if ($callable) {
-                $postItem = $callable($post, $postItem);
-            }
-
-            $postList[] = $postItem;
-        }
-
-        return [
-            'posts' => $posts,
-            'data' => $postList,
-        ];
-    }
-
-    public function getAllFollow()
-    {
-        // 自己的帖子
-        $userIds = [$this->userId];
-        // 我关注的用户的帖子
-        $followerUserIds = $this->getFollowIdsByType(UserFollow::TYPE_USER);
-        $followerUserIds = array_merge($userIds, $followerUserIds);
-
-        $postQueryFollowers = Post::whereIn('user_id', $followerUserIds)->latest();
-
-        // 我关注的小组中的二级精华帖子
-        $followerGroupIds = $this->getFollowIdsByType(UserFollow::TYPE_GROUP);
-        $postQueryGroups = Post::whereNotIn('user_id', $followerUserIds)->whereIn('group_id', $followerGroupIds)->whereIn('digest_state', [2, 3])->latest();
-
-        // 我关注的话题的二级精华帖子
-        $followerIds = $this->getFollowIdsByType(UserFollow::TYPE_HASHTAG);
-        $hashtagLinkeds = $this->getHashtagLinkedByFollowers($followerIds);
-        $hashtagPostIds = $hashtagLinkeds->pluck('linked_id')->toArray();
-        
-        $postQueryHashtags = Post::whereNotIn('user_id', $followerUserIds)->whereNotIn('group_id', $followerGroupIds)->whereIn('id', $hashtagPostIds)->whereIn('digest_state', [2, 3])->latest();
-
-        // 全站二级精华帖子
-        $postQuerySites = Post::whereNotIn('user_id', $followerUserIds)->whereNotIn('group_id', $followerGroupIds)->whereNotIn('id', $hashtagPostIds)->where('digest_state', 3)->latest();
-
-        // 查询数据
-        $posts = $postQuerySites
-            ->union($postQueryFollowers)
-            ->union($postQueryGroups)
-            ->union($postQueryHashtags)
-            ->beforeExpiredAtOrNotLimit($this->user)
-            ->latest()
-            ->paginate(1000);
-
-        return $this->getPostList($posts, null, function ($post, $postItem) use ($followerUserIds, $followerGroupIds, $hashtagPostIds) {
-            $followType = match (true) {
-                default => 'unknown',
-                in_array($post->user_id, $followerUserIds) => 'user',
-                in_array($post->group_id, $followerGroupIds) => 'group',
-                in_array($post->id, $hashtagPostIds) => 'hashtag',
-                in_array($post->digest_state, [2, 3]) => 'digest',
-            };
-
-            $postItem['followType'] = $followType;
-            $postItem['user_id'] = $post->user_id;
-            $postItem['group_id'] = $post->group_id;
-            $postItem['id'] = $post->id;
-            $postItem['digest_state'] = $post->digest_state;
-            $postItem['created_at'] = $post->created_at->toDateTimeString();
-
-            return $postItem;
-        }, $hashtagLinkeds);
-    }
-
-    public function getUserFollow()
-    {
-        $followerIds = $this->getFollowIdsByType(UserFollow::TYPE_USER);
-
-        $followerIds = array_merge($followerIds, [$this->userId]);
-
-        $posts = Post::query()
-            ->whereIn('user_id', $followerIds)
-            ->beforeExpiredAtOrNotLimit($this->user)
-            ->latest()
-            ->paginate($this->dtoRequest->pageSize ?? 15);
-
-        return $this->getPostList($posts, 'user');
-    }
-
-    public function getGroupFollow()
-    {
-        $followerIds = $this->getFollowIdsByType(UserFollow::TYPE_GROUP);
-
-        $posts = Post::query()
-            ->whereIn('group_id', $followerIds)
-            ->beforeExpiredAtOrNotLimit($this->user)
-            ->latest()
-            ->paginate($this->dtoRequest->pageSize ?? 15);
-
-        return $this->getPostList($posts, 'group');
-    }
-
-    public function getHashtagFollow()
-    {
-        // todo: 获取话题帖子功能需要验证。账号下没有数据，暂时不确定功能是否正常。
-        // 获取用户关注的话题
-        // 获取话题下的所有帖子
-        $followerIds = $this->getFollowIdsByType(UserFollow::TYPE_HASHTAG);
-
-        $hashtagLinkeds = $this->getHashtagLinkedByFollowers($followerIds);
-        $hashtagPostIds = $hashtagLinkeds->pluck('linked_id')->toArray();
-
-        $posts = Post::query()
-            ->whereIn('id', $hashtagPostIds)
-            ->beforeExpiredAtOrNotLimit($this->user)
-            ->latest()
-            ->paginate($this->dtoRequest->pageSize ?? 15);
-
-        return $this->getPostList($posts, 'hashtag', null, $hashtagLinkeds);
-    }
-
-    protected function getFollowIdsByType(int $type)
-    {
-        return UserFollow::query()
-            ->when($this->userId, function ($query, $userId) {
-                $query->where('user_id', $userId)->latest();
+        // follow user post
+        $userPostQuery = Post::with('hashtags')
+            ->whereIn('user_id', $allUserIds)
+            ->where(function ($query) use ($blockPostIds, $filterGroupIdsArr) {
+                $query
+                    ->whereNotIn('id', $blockPostIds)
+                    ->orWhereNotIn('group_id', $filterGroupIdsArr);
             })
-            ->type($type)
-            ->pluck('follow_id')
-            ->toArray();
+            ->isEnable()
+            ->latest();
+        $userPostQuery->whereHas('hashtags', function ($query) use ($blockHashtagIds) {
+            $query->whereNotIn('id', $blockHashtagIds);
+        });
+
+        // follow group post
+        $groupPostQuery = Post::with('hashtags')
+            ->where(function ($query) use ($blockPostIds, $allUserIds, $blockUserIds) {
+                $uniqueFilterUserIds = array_unique(array_merge($allUserIds, $blockUserIds));
+
+                $query
+                    ->whereNotIn('id', $blockPostIds)
+                    ->orWhereNotIn('user_id', $uniqueFilterUserIds);
+            })
+            ->whereIn('group_id', $followGroupIds)
+            ->whereIn('digest_state', [2, 3])
+            ->isEnable()
+            ->latest();
+        $groupPostQuery->whereHas('hashtags', function ($query) use ($blockHashtagIds) {
+            $query->whereNotIn('id', $blockHashtagIds);
+        });
+
+        // follow hashtag post
+        $hashtagPostQuery = Post::with('hashtags')
+            ->where(function ($query) use ($blockPostIds, $allUserIds, $blockUserIds, $followGroupIds, $filterGroupIdsArr) {
+                $uniqueFilterUserIds = array_unique(array_merge($allUserIds, $blockUserIds));
+                $uniqueFilterGroupIds = array_unique(array_merge($followGroupIds, $filterGroupIdsArr));
+
+                $query
+                    ->whereNotIn('id', $blockPostIds)
+                    ->orWhereNotIn('user_id', $uniqueFilterUserIds)
+                    ->orWhereNotIn('group_id', $uniqueFilterGroupIds);
+            })
+            ->whereIn('digest_state', [2, 3])
+            ->isEnable()
+            ->latest();
+        $hashtagPostQuery->whereHas('hashtags', function ($query) use ($followHashtagIds) {
+            $query->whereIn('id', $followHashtagIds);
+        });
+
+        // digest post query
+        $digestPostQuery = Post::with('hashtags')
+            ->where(function ($query) use ($blockPostIds, $allUserIds, $followGroupIds, $filterGroupIdsArr) {
+                $uniqueFilterGroupIds = array_unique(array_merge($followGroupIds, $filterGroupIdsArr));
+
+                $query
+                    ->whereNotIn('id', $blockPostIds)
+                    ->orWhereNotIn('user_id', $allUserIds)
+                    ->orWhereNotIn('group_id', $uniqueFilterGroupIds);
+            })
+            ->where('digest_state', 3)
+            ->latest();
+        $digestPostQuery->whereHas('hashtags', function ($query) use ($followHashtagIds) {
+            $query->whereNotIn('id', $followHashtagIds);
+        });
+
+        if (! empty($contentType)) {
+            $userPostQuery->where('types', 'like', "%$contentType%");
+            $groupPostQuery->where('types', 'like', "%$contentType%");
+            $hashtagPostQuery->where('types', 'like', "%$contentType%");
+            $digestPostQuery->where('types', 'like', "%$contentType%");
+        }
+
+        if (! empty($dateLimit)) {
+            $userPostQuery->where('created_at', '<=', $dateLimit);
+            $groupPostQuery->where('created_at', '<=', $dateLimit);
+            $hashtagPostQuery->where('created_at', '<=', $dateLimit);
+            $digestPostQuery->where('created_at', '<=', $dateLimit);
+        }
+
+        $posts = $userPostQuery
+            ->union($groupPostQuery)
+            ->union($hashtagPostQuery)
+            ->union($digestPostQuery)
+            ->latest()
+            ->paginate(\request()->get('pageSize', 15));
+
+        return $posts;
     }
 
-    protected function getHashtagLinkedByFollowers(array $followerIds)
+    // get post list by follow users
+    public function getPostListByFollowUsers(int $authUserId, ?string $contentType = null, ?string $dateLimit = null)
     {
-        return HashtagLinked::whereIn('hashtag_id', $followerIds)->where('linked_type', HashtagLinked::TYPE_POST)->get();
+        $followUserIds = UserFollow::type(UserFollow::TYPE_USER)->where('user_id', $authUserId)->pluck('follow_id')->toArray();
+        $allUserIds = Arr::prepend($followUserIds, $authUserId);
+        $filterGroupIds = PermissionUtility::getGroupPostFilterIds($authUserId);
+        $blockPostIds = UserBlock::type(UserBlock::TYPE_POST)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+
+        $postQuery = Post::whereIn('user_id', $allUserIds)
+            ->whereNotIn('id', $blockPostIds)
+            ->orWhereNotIn('group_id', $filterGroupIds)
+            ->where('is_anonymous', 0)
+            ->isEnable()
+            ->latest();
+
+        if (! empty($contentType)) {
+            $postQuery->where('types', 'like', "%$contentType%");
+        }
+
+        if (! empty($dateLimit)) {
+            $postQuery->where('created_at', '<=', $dateLimit);
+        }
+
+        $posts = $postQuery->paginate(\request()->get('pageSize', 15));
+
+        return $posts;
+    }
+
+    // get post list by follow groups
+    public function getPostListByFollowGroups(int $authUserId, ?string $contentType = null, ?string $dateLimit = null)
+    {
+        $followGroupIds = UserFollow::type(UserFollow::TYPE_GROUP)->where('user_id', $authUserId)->pluck('follow_id')->toArray();
+        $blockUserIds = UserBlock::type(UserBlock::TYPE_USER)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+        $blockHashtagIds = UserBlock::type(UserBlock::TYPE_HASHTAG)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+        $blockPostIds = UserBlock::type(UserBlock::TYPE_POST)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+
+        $postQuery = Post::whereIn('group_id', $followGroupIds)
+            ->where(function ($query) use ($blockPostIds, $blockUserIds) {
+                $query
+                    ->whereNotIn('id', $blockPostIds)
+                    ->orWhereNotIn('user_id', $blockUserIds);
+            })
+            ->isEnable()
+            ->latest();
+        $postQuery->whereHas('hashtags', function ($query) use ($blockHashtagIds) {
+            $query->whereNotIn('id', $blockHashtagIds);
+        });
+
+        if (! empty($contentType)) {
+            $postQuery->where('types', 'like', "%$contentType%");
+        }
+
+        if (! empty($dateLimit)) {
+            $postQuery->where('created_at', '<=', $dateLimit);
+        }
+
+        $posts = $postQuery->paginate(\request()->get('pageSize', 15));
+
+        return $posts;
+    }
+
+    // get post list by follow hashtags
+    public function getPostListByFollowHashtags(int $authUserId, ?string $contentType = null, ?string $dateLimit = null)
+    {
+        $followHashtagIds = UserFollow::type(UserFollow::TYPE_HASHTAG)->where('user_id', $authUserId)->pluck('follow_id')->toArray();
+        $blockUserIds = UserBlock::type(UserBlock::TYPE_USER)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+        $blockGroupIds = UserBlock::type(UserBlock::TYPE_GROUP)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+        $modeGroupIds = PermissionUtility::getGroupPostFilterIds($authUserId);
+        $filterGroupIds = Arr::prepend($blockGroupIds, $modeGroupIds);
+        $blockPostIds = UserBlock::type(UserBlock::TYPE_POST)->where('user_id', $authUserId)->pluck('block_id')->toArray();
+
+        $postQuery = Post::with('hashtags')
+            ->where(function ($query) use ($blockPostIds, $blockUserIds, $filterGroupIds) {
+                $query
+                    ->whereNotIn('id', $blockPostIds)
+                    ->orWhereNotIn('user_id', $blockUserIds)
+                    ->orWhereNotIn('group_id', $filterGroupIds);
+            })
+            ->isEnable()
+            ->latest();
+        $postQuery->whereHas('hashtags', function ($query) use ($followHashtagIds) {
+            $query->whereIn('id', $followHashtagIds);
+        });
+
+        if (! empty($contentType)) {
+            $postQuery->where('types', 'like', "%$contentType%");
+        }
+
+        if (! empty($dateLimit)) {
+            $postQuery->where('created_at', '<=', $dateLimit);
+        }
+
+        $posts = $postQuery->paginate(\request()->get('pageSize', 15));
+
+        return $posts;
     }
 }
