51052189413051c570b72ec88eca82b6f1f9f85d
Jarvis <jarvis.okay@gmail.com>
2022-06-12 22:13:45 +0800

    feat(api): 会话和消息

--- app/Fresns/Api/Http/DTO/DialogSendMessageDTO.php
+++ app/Fresns/Api/Http/DTO/DialogSendMessageDTO.php
@@ -10,7 +10,7 @@ namespace App\Fresns\Api\Http\DTO;
 
 use Fresns\DTO\DTO;
 
-class PaginationDTO extends DTO
+class DialogSendMessageDTO extends DTO
 {
     /**
      * @return array
@@ -18,8 +18,9 @@ class PaginationDTO extends DTO
     public function rules(): array
     {
         return [
-            'pageSize' => ['integer', 'nullable', 'between:1,30'],
-            'page' => ['integer', 'nullable'],
+            'uidOrUsername' => ['string', 'required'],
+            'message' => ['string', 'nullable', 'required_without:fid'],
+            'fid' => ['string', 'nullable', 'required_without:message'],
         ];
     }
 }
