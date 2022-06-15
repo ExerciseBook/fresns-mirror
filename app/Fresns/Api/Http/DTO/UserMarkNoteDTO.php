87d478c58376c5cdc0050699b921576a8f66a105
Jarvis <jarvis.okay@gmail.com>
2022-06-15 13:25:05 +0800

    feat(api): 对标记对象备注 dto

---
+++ app/Fresns/Api/Http/DTO/UserMarkNoteDTO.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jarvis Tang
+ * Released under the Apache-2.0 License.
+ */
+
+namespace App\Fresns\Api\Http\DTO;
+
+use Fresns\DTO\DTO;
+
+class UserMarkNoteDTO extends DTO
+{
+    /**
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'interactiveType' => ['string', 'required', 'in:follow,block'],
+            'markType' => ['string', 'required', 'in:user,group,hashtag,post,comment'],
+            'fsid' => ['string', 'required'],
+            'note' => ['string', 'nullable'],
+        ];
+    }
+}
