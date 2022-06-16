5c534fa4e81dedf0278463b50f9435f4c043de8b
mouyong <my24251325@gmail.com>
2022-06-16 22:19:01 +0800

    feat: 编码风格注释

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -12,6 +12,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * 编码风格
+ * 
+ class XxxModel
+{
+    // const 常量定义区域
+    // use Trait 定义区域
+    // static 属性定义区域
+    // 属性定义区域
+    // static 父级 Model 函数定义区域
+    // scope 函数定义区域
+    // setXxxYyyAttribute 定义区域
+    // getXxxYyyAttribute 定义区域
+    // xxxYyy(): Attribute 定义区域
+    // relations 关系定义区域
+    // 自定义函数定义区域
+    // 静态自定义函数定义区域
+}
+ */
 class Model extends BaseModel
 {
     use HasFactory;
