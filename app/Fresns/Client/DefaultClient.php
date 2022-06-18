c6472584a81e76b76f183e8015afbd0abce18151
mouyong <my24251325@gmail.com>
2022-06-18 13:09:10 +0800

    feat: 添加默认的 http client

---
+++ app/Fresns/Client/DefaultClient.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jarvis Tang
+ */
+
+namespace App\Fresns\Client;
+
+use Psr\Http\Message\ResponseInterface;
+
+trait DefaultClient
+{
+    public function getBaseUri(): ?string
+    {
+        return null;
+    }
+
+    public function handleEmptyResponse(?string $content = null, ?ResponseInterface $response = null)
+    {
+        return null;
+    }
+
+    public function isErrorResponse(array $data): bool
+    {
+        return false;
+    }
+
+    public function handleErrorResponse(?string $content = null, array $data = [])
+    {
+        return null;
+    }
+
+    public function hasPaginate(): bool
+    {
+        return false;
+    }
+
+    public function getTotal(): ?int
+    {
+        return 0;
+    }
+
+    public function getPageSize(): ?int
+    {
+        return 0;
+    }
+
+    public function getCurrentPage(): ?int
+    {
+        return 0;
+    }
+
+    public function getLastPage(): ?int
+    {
+        return 0;
+    }
+
+    public function getDataList(): static|array|null
+    {
+        return null;
+    }
+}
