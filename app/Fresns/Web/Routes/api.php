9b3153eb02369853a4ac6ac6f60358e82a5b6afe
mouyong <my24251325@gmail.com>
2022-07-16 16:50:35 +0800

    feat: 修复 web 端，接口错误信息不提示问题

--- app/Fresns/Web/Routes/api.php
+++ app/Fresns/Web/Routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('engine')
     ->middleware([
+        'web',
         AccountAuthorize::class,
         UserAuthorize::class,
         CheckSiteModel::class,
