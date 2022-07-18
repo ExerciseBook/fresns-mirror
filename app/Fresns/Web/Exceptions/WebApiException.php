2eccb29b5396496dc9a05283296436b073ba5bf9
mouyong <my24251325@gmail.com>
2022-07-18 16:55:32 +0800

    fix: 修复接口数据序列化失败问题

--- app/Fresns/Web/Exceptions/WebApiException.php
+++ app/Fresns/Web/Exceptions/WebApiException.php
@@ -9,27 +9,8 @@
 namespace App\Fresns\Web\Exceptions;
 
 use App\Fresns\Api\Traits\ApiResponseTrait;
-use App\Helpers\ConfigHelper;
-use App\Utilities\ConfigUtility;
 
 class WebApiException extends \Exception
 {
     use ApiResponseTrait;
-
-    public function __construct(int $code, ?string $unikey = '')
-    {
-        $message = $this->getCodeMessage($code, $unikey);
-
-        parent::__construct($message, $code);
-    }
-
-    public function getCodeMessage(int $code, ?string $unikey = '')
-    {
-        return ConfigUtility::getCodeMessage($code, $unikey, \request()->header('langTag', ConfigHelper::fresnsConfigDefaultLangTag()));
-    }
-
-    public function render()
-    {
-        return $this->failure($this->getCode(), $this->getMessage());
-    }
 }
