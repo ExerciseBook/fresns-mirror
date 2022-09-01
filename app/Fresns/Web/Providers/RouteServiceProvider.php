dc18c495f7754cf1237a46dc53589d8a509e29ec
mouyong <my24251325@gmail.com>
2022-09-01 09:53:25 +0800

    fix: 修复 php artisan serve 不能访问页面问题

--- app/Fresns/Web/Providers/RouteServiceProvider.php
+++ app/Fresns/Web/Providers/RouteServiceProvider.php
@@ -31,6 +31,14 @@ class RouteServiceProvider extends ServiceProvider
 
         $url = config('app.url');
         $host = str_replace(['http://', 'https://'], '', rtrim($url, '/'));
+        $currentAccessHost = \request()->httpHost();
+
+        // 通过 php artisan serve 启动时，app.url 如果与访问访问域名不同，则不设置路由的 domain 限制。
+        // 解决的问题：插件直达域名首页。允许插件自定义域名首页。
+        // 原理：同一路由，后面的会覆盖前面的。通过 domain 限制，可以达到效果。
+        if ($host != $currentAccessHost) {
+            $host = null;
+        }
 
         Route::group([
             'domain' => $host,
