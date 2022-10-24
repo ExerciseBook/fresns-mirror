9b611e79eba136925f216cbbf65b33a200596f1b
Jarvis <jarvis.okay@gmail.com>
2022-10-24 22:25:09 +0800

    fix(helper): 缓存 key

--- app/Helpers/PluginHelper.php
+++ app/Helpers/PluginHelper.php
@@ -58,7 +58,7 @@ class PluginHelper
      */
     public static function fresnsPluginUsageUrl(string $unikey, ?string $parameter = null)
     {
-        $parameterKey = StrHelper::slug($parameter);
+        $parameterKey = ! empty($parameter) ? StrHelper::slug($parameter) : 'usage';
         $cacheKey = 'fresns_plugin_'.$unikey.'_'.$parameterKey.'_url';
 
         $pluginUrl = Cache::remember($cacheKey, now()->addDays(7), function () use ($unikey, $parameter) {
