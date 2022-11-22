a5a3ad63046a06eca24920218074a6c955670a37
Jarvis <jarvis.okay@gmail.com>
2022-11-22 09:54:02 +0800

    build: v2.0.0-beta.1

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -19,8 +19,8 @@ class AppHelper
 {
     const VERSION = '2.0.0-beta.1';
     const VERSION_INT = 1;
-    const VERSION_MD5 = md5(AppHelper::VERSION);
-    const VERSION_MD5_16BIT = substr(AppHelper::VERSION_MD5, 8, 16);
+    const VERSION_MD5 = 'ad08a1e2f72f73db450864a89d753a67';
+    const VERSION_MD5_16BIT = 'f72f73db450864a8';
 
     // fresns test helper
     public static function fresnsTestHelper()
