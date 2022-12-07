a5ccc9eadb203009b96e985cae12e912e5706617
Jarvis <jarvis.okay@gmail.com>
2022-12-07 16:55:25 +0800

    refactor(words): 账号和用户 token 命令字

--- app/Fresns/Words/User/User.php
+++ app/Fresns/Words/User/User.php
@@ -9,17 +9,24 @@
 namespace App\Fresns\Words\User;
 
 use App\Fresns\Words\User\DTO\AddUserDTO;
+use App\Fresns\Words\User\DTO\CreateUserTokenDTO;
 use App\Fresns\Words\User\DTO\LogicalDeletionUserDTO;
 use App\Fresns\Words\User\DTO\VerifyUserDTO;
+use App\Fresns\Words\User\DTO\VerifyUserTokenDTO;
+use App\Helpers\CacheHelper;
 use App\Helpers\ConfigHelper;
+use App\Helpers\PrimaryHelper;
 use App\Models\Account;
 use App\Models\Conversation;
 use App\Models\File;
+use App\Models\SessionToken;
 use App\Models\User as UserModel;
 use App\Models\UserRole;
 use App\Models\UserStat;
 use App\Utilities\ConfigUtility;
+use Carbon\Carbon;
 use Fresns\CmdWordManager\Traits\CmdWordResponseTrait;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
 
 class User
@@ -128,6 +135,156 @@ class User
         $data['uid'] = $user->uid;
 
         return $this->success($data);
+    }
+
+    /**
+     * @param $wordBody
+     * @return array
+     *
+     * @throws \Throwable
+     */
+    public function createUserToken($wordBody)
+    {
+        $dtoWordBody = new CreateUserTokenDTO($wordBody);
+
+        $verifyAccountToken = \FresnsCmdWord::plugin()->verifyAccountToken($wordBody);
+
+        if ($verifyAccountToken->isErrorResponse()) {
+            return $verifyAccountToken->errorResponse();
+        }
+
+        $accountId = PrimaryHelper::fresnsAccountIdByAid($dtoWordBody->aid);
+        $userId = PrimaryHelper::fresnsUserIdByUidOrUsername($dtoWordBody->uid);
+
+        $userTokenModel = SessionToken::where('app_id', $dtoWordBody->appId)
+            ->where('account_id', $accountId)
+            ->where('account_token', $dtoWordBody->aidToken)
+            ->where('user_id', $userId)
+            ->first();
+
+        if ($userTokenModel) {
+            if (empty($userTokenModel->expired_at) || $userTokenModel->expired_at->greaterThan(Carbon::now())) {
+                $expiredHours = null;
+                $expiredDays = null;
+                $expiredDateTime = null;
+                if ($userTokenModel->expired_at) {
+                    $expiredHours = $userTokenModel->expired_at->diffInHours(Carbon::now());
+                    $expiredDays = $userTokenModel->expired_at->diffInDays(Carbon::now());
+                    $expiredDateTime = date('Y-m-d H:i:s', $userTokenModel->expired_at);
+                }
+
+                return $this->success([
+                    'aid' => $dtoWordBody->aid,
+                    'aidToken' => $dtoWordBody->aidToken,
+                    'uid' => $dtoWordBody->uid,
+                    'uidToken' => $userTokenModel->user_token,
+                    'uidTokenId' => $userTokenModel->id,
+                    'expiredHours' => $expiredHours,
+                    'expiredDays' => $expiredDays,
+                    'expiredDateTime' => $expiredDateTime,
+                ]);
+            }
+        }
+
+        $token = \Str::random(32);
+        $expiredHours = null;
+        $expiredDays = null;
+        $expiredDateTime = null;
+        if ($dtoWordBody->expiredTime) {
+            $now = time();
+            $time = $dtoWordBody->expiredTime * 3600;
+            $expiredTime = $now + $time;
+
+            $dt = Carbon::parse($expiredTime);
+
+            $expiredHours = $dtoWordBody->expiredTime;
+            $expiredDays = $dt->diffInDays(Carbon::now());
+            $expiredDateTime = date('Y-m-d H:i:s', $expiredTime);
+        }
+
+        $condition = [
+            'platform_id' => $dtoWordBody->platformId,
+            'version' => $dtoWordBody->version,
+            'appId' => $dtoWordBody->appId,
+            'account_id' => $accountId,
+            'account_token' => $dtoWordBody->aidToken,
+            'user_id' => $userId,
+            'user_token' => $token,
+            'expired_at' => $expiredDateTime,
+        ];
+
+        $tokenModel = SessionToken::create($condition);
+
+        return $this->success([
+            'aid' => $dtoWordBody->aid,
+            'aidToken' => $dtoWordBody->aidToken,
+            'uid' => $dtoWordBody->uid,
+            'uidToken' => $token,
+            'uidTokenId' => $tokenModel->id,
+            'expiredHours' => $expiredHours,
+            'expiredDays' => $expiredDays,
+            'expiredDateTime' => $expiredDateTime,
+        ]);
+    }
+
+    /**
+     * @param $wordBody
+     * @return array
+     *
+     * @throws \Throwable
+     */
+    public function verifyUserToken($wordBody)
+    {
+        $dtoWordBody = new VerifyUserTokenDTO($wordBody);
+
+        $verifyAccountToken = \FresnsCmdWord::plugin()->verifyAccountToken($wordBody);
+
+        if ($verifyAccountToken->isErrorResponse()) {
+            return $verifyAccountToken->errorResponse();
+        }
+
+        $langTag = \request()->header('langTag', ConfigHelper::fresnsConfigDefaultLangTag());
+
+        $accountId = PrimaryHelper::fresnsAccountIdByAid($dtoWordBody->aid);
+        $userId = PrimaryHelper::fresnsUserIdByUidOrUsername($dtoWordBody->uid);
+        $uidToken = $dtoWordBody->uidToken;
+        $appId = $dtoWordBody->appId;
+
+        $cacheKey = "fresns_token_user_{$userId}_{$uidToken}";
+        $cacheTime = CacheHelper::fresnsCacheTimeByFileType();
+
+        // Cache::tags(['fresnsSystems'])
+        $userToken = Cache::remember($cacheKey, $cacheTime, function () use ($userId, $uidToken, $appId) {
+            return SessionToken::where('app_id', $appId)
+                ->where('user_id', $userId)
+                ->where('user_token', $uidToken)
+                ->first();
+        });
+
+        if (is_null($userToken)) {
+            Cache::forget($cacheKey);
+
+            return $this->failure(
+                31603,
+                ConfigUtility::getCodeMessage(31603, 'Fresns', $langTag)
+            );
+        }
+
+        if ($userToken->platform_id != $dtoWordBody->platformId) {
+            return $this->failure(
+                31103,
+                ConfigUtility::getCodeMessage(31103, 'Fresns', $langTag)
+            );
+        }
+
+        if ($userToken->expired_at && $userToken->expired_at < now()) {
+            return $this->failure(
+                31504,
+                ConfigUtility::getCodeMessage(31504, 'Fresns', $langTag)
+            );
+        }
+
+        return $this->success();
     }
 
     /**
