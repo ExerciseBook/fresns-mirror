ae62eac9dab6b5d72a2472e5ceca3c04eb980859
Jarvis <jarvis.okay@gmail.com>
2022-12-07 15:18:25 +0800

    chore(data): token 表

---
+++ database/migrations/2022_12_07_090900_change_session_tokens_table.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jarvis Tang
+ * Released under the Apache-2.0 License.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeSessionTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (! Schema::hasColumn('session_tokens', 'user_token')) {
+            Schema::table('session_tokens', function (Blueprint $table) {
+                $table->dropUnique('account_token');
+
+                $table->string('version', 16)->nullable()->after('platform_id');
+                $table->char('app_id', 8)->nullable()->after('platform_id');
+                $table->char('user_token', 32)->nullable()->after('user_id');
+
+                Schema::rename('token', 'account_token');
+
+                $table->unique(['account_id', 'account_token'], 'account_token');
+                $table->unique(['user_id', 'user_token'], 'user_token');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('session_tokens', 'user_token')) {
+            Schema::table('session_tokens', function (Blueprint $table) {
+                $table->dropColumn('version');
+                $table->dropColumn('app_id');
+                $table->dropColumn('account_token');
+
+                Schema::rename('account_token', 'token');
+
+                $table->dropUnique('account_token');
+                $table->dropUnique('user_token');
+            });
+        }
+    }
+}
