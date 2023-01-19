fcecd4c6c46a2af147595976fc80381f70573ebb
Jarvis <jarvis.okay@gmail.com>
2023-01-19 11:15:56 +0800

    chore(helper): sign

--- app/Helpers/SignHelper.php
+++ app/Helpers/SignHelper.php
@@ -24,10 +24,20 @@ class SignHelper
     // Check Sign
     public static function checkSign(array $signMap, string $appSecret): bool
     {
-        $inputSign = $signMap['X-Fresns-Signature'];
-        unset($signMap['X-Fresns-Signature']);
+        $checkArr = [
+            'X-Fresns-App-Id' => $signMap['X-Fresns-App-Id'] ?? $signMap['appId'] ?? null,
+            'X-Fresns-Client-Platform-Id' => $signMap['X-Fresns-Client-Platform-Id'] ?? $signMap['platformId'] ?? null,
+            'X-Fresns-Client-Version' => $signMap['X-Fresns-Client-Version'] ?? $signMap['version'] ?? null,
+            'X-Fresns-Aid' => $signMap['X-Fresns-Aid'] ?? $signMap['aid'] ?? null,
+            'X-Fresns-Aid-Token' => $signMap['X-Fresns-Aid-Token'] ?? $signMap['aidToken'] ?? null,
+            'X-Fresns-Uid' => $signMap['X-Fresns-Uid'] ?? $signMap['uid'] ?? null,
+            'X-Fresns-Uid-Token' => $signMap['X-Fresns-Uid-Token'] ?? $signMap['uidToken'] ?? null,
+            'X-Fresns-Signature-Timestamp' => $signMap['X-Fresns-Signature-Timestamp'] ?? $signMap['timestamp'] ?? null,
+        ];
 
-        $makeSign = SignHelper::makeSign($signMap, $appSecret);
+        $inputSign = $signMap['X-Fresns-Signature'] ?? $signMap['signature'];
+
+        $makeSign = SignHelper::makeSign($checkArr, $appSecret);
 
         return $inputSign == $makeSign;
     }
