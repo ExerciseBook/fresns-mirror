821607892309dd243556b45dab260467666ebdee
Jarvis <jarvis.okay@gmail.com>
2023-01-21 11:24:19 +0800

    chore(console): 优化升级命令

--- app/Console/Commands/Upgrade/Upgrade13Command.php
+++ app/Console/Commands/Upgrade/Upgrade13Command.php
@@ -9,7 +9,6 @@
 namespace App\Console\Commands\Upgrade;
 
 use Illuminate\Console\Command;
-use Symfony\Component\Process\Process;
 
 class Upgrade13Command extends Command
 {
@@ -25,44 +24,9 @@ class Upgrade13Command extends Command
     // execute the console command
     public function handle()
     {
-        logger('upgrade:fresns-13 composerInstall');
-        $this->composerInstall();
-
         logger('upgrade:fresns-13 migrate');
         $this->call('migrate', ['--force' => true]);
 
         return Command::SUCCESS;
-    }
-
-    // composer install
-    public function composerInstall()
-    {
-        $composerPath = 'composer';
-
-        if (! $this->commandExists($composerPath)) {
-            $composerPath = '/usr/bin/composer';
-        }
-
-        $process = new Process([$composerPath, 'install'], base_path());
-        $process->setTimeout(0);
-        $process->start();
-
-        foreach ($process as $type => $data) {
-            if ($process::OUT === $type) {
-                $this->info("\nRead from stdout: ".$data);
-            } else { // $process::ERR === $type
-                $this->info("\nRead from stderr: ".$data);
-            }
-        }
-    }
-
-    // check composer
-    public function commandExists($commandName)
-    {
-        ob_start();
-        passthru("command -v $commandName", $code);
-        ob_end_clean();
-
-        return (0 === $code) ? true : false;
     }
 }
